7414a16271106816a99b375e7d2fadcf778ae954
IscarioteSXIII <jul.degraeve@gmail.com>
2022-06-11 21:13:54 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
+##Spoofing Navigateur WEB
+
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0"}
 
 
